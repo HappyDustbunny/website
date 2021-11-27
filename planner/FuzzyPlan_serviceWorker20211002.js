f11cd2d0d4fdb15d3dbad993122325f3d016b191
HappyDustbunny <madshorn@operamail.com>
2021-11-27 13:28:45 +0100

    Fix bug with cache matching. Maybe

--- planner/FuzzyPlan_serviceWorker20211002.js
+++ planner/FuzzyPlan_serviceWorker20211002.js
@@ -5,17 +5,17 @@ let CACHED_URLS = [
   '200px-A_SVG_semicircle_heart.svg.png',
   '200px-Flag_of_Denmark.png',
   '200px-Flag_of_the_United_Kingdom.png',
-  'icon.svg',
-  'favicon.ico',
-  'apple-touch-icon.png',
-  'favicons/maskable_192.png',
-  'favicons/favicon-512.png',
   'FuzzyPlan20211002.css',
   'FuzzyPlan20211002.js',
+  'apple-touch-icon.png',
+  'favicon.ico',
+  'icon.svg',
+  'index.html'
   'instructions_dk.html',
   'instructions.html',
   'manifest.json',
-  'index.html'
+  'favicons/maskable_192.png',
+  'favicons/favicon-512.png',
 ];
 
 self.addEventListener('install', function(event) {
@@ -28,37 +28,11 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   console.log('Fetch request for: ', event.request.url);
-  event.respondWith( caches.match(event.request).then(
+  event.respondWith( caches.match(event.request, {ignoreVary: true}).then( // ignoreVary should make the cache match ignore flags and stuff that can make a mathc fail unintentionally
     function(response) {
       return response || fetch(event.request);
     }).catch(function(error) {
-      return caches.match('index.html')
+      console.log('FuzzyPlan serviceWorker respondWith error', error);
     })
   );
 })
-
-
-// 'FPlogo.png',
-// 'favicons/browserconfig.xml',
-
-// 'favicons/favicon-16.png',
-// 'favicons/favicon-32.png',
-// 'favicons/favicon-57.png',
-// 'favicons/favicon-60.png',
-// 'favicons/favicon-64.png',
-// 'favicons/favicon-64maskable.png',
-// 'favicons/favicon-70.png',
-// 'favicons/favicon-72.png',
-// 'favicons/favicon-76.png',
-// 'favicons/favicon-96.png',
-// 'favicons/favicon-114.png',
-// 'favicons/favicon-120.png',
-// 'favicons/favicon-144.png',
-// 'favicons/favicon-150.png',
-// 'favicons/favicon-152.png',
-// 'favicons/favicon-160.png',
-// 'favicons/favicon-180.png',
-// 'favicons/favicon-310.png',
-
-// 'favicons/favicon.ico',
-// 'favicons/favicon.png',
